b89a6a10e2a1beafe893faa6eec6e2562ded7492
sukrit007 <sukrit007@gmail.com>
2014-10-16 11:41:03 -0700

    Set concurrency to 2 for local

--- local-celery.py
+++ local-celery.py
@@ -3,4 +3,5 @@ from deployer.celery import app
 
 if __name__ == '__main__':
     argv = sys.argv if len(sys.argv) > 1 else [__file__, '--loglevel=info']
+    app.conf.CELERYD_CONCURRENCY = 2
     app.worker_main(argv=argv)
