a57cbeb725f36400e9c5ad0795c2ccd2fee9628d
sukrit007 <sukrit007@gmail.com>
2014-10-16 20:32:10 -0700

    Fix integration issues while updating keys

--- bin/confd-wrapper.sh
+++ bin/confd-wrapper.sh
@@ -6,5 +6,5 @@ ETCDCTL="etcdctl --peers $ETCD_URL"
 $ETCDCTL get $ETCD_PROXY_BASE/syslog/host || $ETCDCTL set $ETCD_PROXY_BASE/syslog/host ""
 
 
-sed -i -e "s/127.0.0.1[:]4001/$ETCD_URL/g" -e "s|/totem|$ETCD_PROXY_BASE|g" /etc/confd/confd.toml
+sed -i -e "s/172.17.42.1[:]4001/$ETCD_URL/g" -e "s|/totem|$ETCD_PROXY_BASE|g" /etc/confd/confd.toml
 confd
