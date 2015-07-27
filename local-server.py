55c1c558c13e236479944aa802d48f9c78933d1e
sukrit007 <sukrit007@gmail.com>
2015-07-27 03:08:23 -0700

    Recovery API

--- local-server.py
+++ local-server.py
@@ -6,6 +6,6 @@ from deployer.server import app
 if __name__ == '__main__':
     app.run(debug=os.getenv('FLASK_DEBUG', 'false').strip().lower() in
             BOOLEAN_TRUE_VALUES,
-            threaded=True,
+            threaded=False,
             host='0.0.0.0',
             port=API_PORT)
